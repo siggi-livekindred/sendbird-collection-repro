185cc07fac633d489d064147b36de85483e8d8ff
Siggi Jonsson <siggi@livekindred.com>
2024-02-13 10:40:29 -0800

    Mirror error handling in GroupChannelScreen

--- screens/GroupChannelScreen.tsx
+++ screens/GroupChannelScreen.tsx
@@ -1,6 +1,8 @@
 import {useNavigation, useRoute} from '@react-navigation/native';
 import {createGroupChannelFragment, useSendbirdChat,} from '@sendbird/uikit-react-native';
 import {useGroupChannel} from '@sendbird/uikit-chat-hooks';
+import {useEffect} from "react";
+import {Text, View} from "react-native";
 
 const GroupChannelFragment = createGroupChannelFragment();
 
@@ -9,7 +11,21 @@ export const GroupChannelScreen = () => {
     const {params} = useRoute<any>();
 
     const {sdk} = useSendbirdChat();
-    const {channel} = useGroupChannel(sdk, params.channelUrl);
+    const {channel, error} = useGroupChannel(sdk, params.channelUrl);
+
+    useEffect(() => {
+        console.error("GroupChannelScreen", error);
+    }, [error]);
+
+    if (error) {
+        return (
+            <View>
+                <Text>Channel not found</Text>
+                <Text>The link you’ve followed is either invalid, or the channel has been removed.</Text>
+            </View>
+        );
+    }
+
     if (!channel) return null;
 
     return (
